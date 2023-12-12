5f11ca4ac92f163a4da5b3773b8bed3de417150a
Gabriel Mango <gabrielhmango@gmail.com>
2023-12-12 14:01:51 -0300

    feat: database connetion

--- src/infra/db/settings/get_envs.py
+++ src/infra/db/settings/get_envs.py
@@ -4,10 +4,15 @@ import dotenv
 
 dotenv.load_dotenv()
 
-DATABASE_STRING = "postgresql+psycopg2://{}:{}@{}:{}/{}".format(
+def get_database_string():
+    return 'postgresql+psycopg2://{}:{}@{}:{}/{}'.format(
     os.environ["DATABASE_USER"],
     os.environ["DATABASE_PASSWORD"],
     os.environ["DATABASE_HOST"],
     os.environ["DATABASE_PORT"],
     os.environ["DATABASE_NAME"],
 )
+
+
+if __name__ == '__main__':
+    print(get_database_string())
